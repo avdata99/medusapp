5e3dd5c557f3ffa47439d01b4267ef7e01a40d14
avdata99 <andres@data99.com.ar>
2014-12-15 20:41:03 -0300

    starting

---
+++ application/views/headers.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?= $title ?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?=$this->config->item('base_url')?>static/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?=$this->config->item('base_url')?>static/css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="<?=$this->config->item('base_url')?>static/css/plugins/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?=$this->config->item('base_url')?>static/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="<?=$this->config->item('base_url')?>static/css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?=$this->config->item('base_url')?>static/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <?php /* for grocery crud */ ?>
+    <?php foreach($css_files as $file): ?>
+        <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+    <?php endforeach; ?>
+
+</head>
+
---
+++ application/views/js_loads.php
@@ -0,0 +1,21 @@
+<?php /* for grocery crud */ ?>
+<?php foreach($js_files as $file): ?>
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+
+<!-- jQuery Version 1.11.0 -->
+<script src="<?=$this->config->item('base_url')?>static/js/jquery-1.11.0.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?=$this->config->item('base_url')?>static/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?=$this->config->item('base_url')?>static/js/plugins/metisMenu/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="<?=$this->config->item('base_url')?>static/js/plugins/morris/raphael.min.js"></script>
+<script src="<?=$this->config->item('base_url')?>static/js/plugins/morris/morris.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?=$this->config->item('base_url')?>static/js/sb-admin-2.js"></script>
+

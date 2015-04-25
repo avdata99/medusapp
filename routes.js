cc8dfc72ec516d5e717dd732fa3929d4b9b0c234
avdata99 <andres@data99.com.ar>
2015-04-25 16:28:52 -0300

    start using routes

--- routes.js
+++ routes.js
@@ -2,13 +2,19 @@ App.Router = Backbone.Router.extend({
   routes: {
     '': 'index',
     // 'withParams/:param1/:param2': 'funcWithParams',
+    'licitacion/:slug': 'licitacion',
     '*other': 'fail404',
   },
 
-  /** mostrar pagina principal */
+  /** mostrar pagina principal con las licitaciones */
   index: function() {
     
     touchAnalytics('/index.html', 'Home');
+    App.Models.licitaciones = new licitacionesCollection();
+    App.Models.licitaciones.getLicitaciones(); // carga todas las licitaciones
+
+    // cargar las licitaciones leidas en la home
+    App.Collections.licitacionesView = new licitacionesCollectionView({collection: App.Models.licitaciones})
     },
 
   /*
@@ -18,7 +24,11 @@ App.Router = Backbone.Router.extend({
         
   },
  */
-  
+
+  licitacion: function(slug){
+
+  },
+
   fail404: function() {
     touchAnalytics('/404', 'Error 404');
     alert("404");

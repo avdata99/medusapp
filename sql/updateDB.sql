258c4ce6d5f6e8fb8141846a69cd905c57a84f26
avdata99 <andres@data99.com.ar>
2015-05-17 11:17:39 -0300

    sql

--- sql/updateDB.sql
+++ sql/updateDB.sql
@@ -1,6 +1,19 @@
 -- left here new sql quries for update the producction server
 -- BE CAREFUL
 
+/* 
+CREATE TABLE `datos_publicar` (`id` int(11) NOT NULL AUTO_INCREMENT,
+ `titulo` varchar(120) NOT NULL, `descripcion` text NOT NULL,  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+INSERT INTO `pactointegridad`.`datos_publicar` (`id`, `titulo`, `descripcion`) 
+  VALUES 
+  (NULL, 'Ultimo balance Anual', 'Fichero adjunto en formato PDF con el ultimo balance aprobado completo'), 
+  (NULL, 'Estatuto social', 'Archivo PDF adjunto con los estatutos de la sociedad'),
+  (NULL, 'Ultimo informe de auditores', 'Archivo adjunto en PDF con el último informe de auditores'), 
+  (NULL, 'Organo de administración', 'Archivo adjunto PDF con la nómina actual de los miembros del órgano de administracion');
+
+
 CREATE TABLE IF NOT EXISTS `social` (
   `social_name` varchar(90) NOT NULL,
   `social_value` varchar(190) NOT NULL,
@@ -18,3 +31,5 @@ INSERT INTO `social` (`social_name`, `social_value`) VALUES
 ('mailchimp_action_url', '//medusapp.us10.list-manage.com/subscribe/post?u=7f72f6878941580f6affb71a0&amp;id=2d22ce24ef'),
 ('mail_show', 'info@medusapp.org'),
 ('twitter_account', '@medusapp');
+
+*/

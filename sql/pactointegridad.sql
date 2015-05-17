100cbb0ef722b29d622c9e5ccea723e7a7004e6b
avdata99 <andres@data99.com.ar>
2015-05-17 00:30:35 -0300

    add mailchimp to social values

--- sql/pactointegridad.sql
+++ sql/pactointegridad.sql
@@ -301,6 +301,25 @@ INSERT INTO `pactointegridad`.`datos_publicar` (`id`, `titulo`, `descripcion`)
   (NULL, 'Ultimo informe de auditores', 'Archivo adjunto en PDF con el último informe de auditores'), 
   (NULL, 'Organo de administración', 'Archivo adjunto PDF con la nómina actual de los miembros del órgano de administracion');
 
+CREATE TABLE IF NOT EXISTS `social` (
+  `social_name` varchar(90) NOT NULL,
+  `social_value` varchar(190) NOT NULL,
+  UNIQUE KEY `social_name` (`social_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `social`
+--
+
+INSERT INTO `social` (`social_name`, `social_value`) VALUES
+('developed_by_txt', 'el Observatorio de Políticas Públicas de la Universidad Católica de Córdoba'),
+('developed_by_url', 'http://www.ucc.edu.ar/portalnuevo/paginaspopup/fichaProyectosSivPublica.php?hash=ba97718297fae089e297ec7f0e6353f4'),
+('facebook_account', 'medusaapp'),
+('mailchimp_action_url', '//medusapp.us10.list-manage.com/subscribe/post?u=7f72f6878941580f6affb71a0&amp;id=2d22ce24ef'),
+('mail_show', 'info@medusapp.org'),
+('twitter_account', '@medusapp');
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

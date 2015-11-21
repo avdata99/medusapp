a01abc0ad45a784979284e704c6c3710319e4ac1
avdata99 <andres@data99.com.ar>
2015-11-21 19:15:31 -0300

    add blog

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ echo "Fix file's and forlder's permissions"
 ssh -p 987 pin2@medusapp.org "find /home/pin2/www/ -type d -exec chmod 755 {} \; && find /home/pin2/www/ -type f -exec chmod 644 {} \;"
 
 # database 
-ssh -p 987 pin2@medusapp.org "mysql -u pin2_user -ppin*2_99 pin2_db < /home/pin2/www/sql/updateDB.sql"
+ssh -p 987 pin2@medusapp.org "mysql -u pin2_user -p pin2_db < /home/pin2/www/sql/updateDB.sql"

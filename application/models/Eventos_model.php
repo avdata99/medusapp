764f30df9d8f9e23651dd331868e87879275ad5c
avdata99 <andres@data99.com.ar>
2015-12-27 19:16:04 -0300

    unfinished

--- application/models/Eventos_model.php
+++ application/models/Eventos_model.php
@@ -3,7 +3,7 @@ class Eventos_model extends CI_Model{
 
 	/* Agregar una notificacion para un usuario especifico */
 	public function add($user_id, $titulo='', $descripcion='', $url=''){
-		$q = "INSERT INTO notificaciones 
+		$q = "INSERT INTO eventos 
 	        (user_id, titulo, descripcion, url, status) 
 	        VALUES 
 	        ($user_id, '$titulo', '$descripcion', '$url', 1);";
@@ -12,8 +12,8 @@ class Eventos_model extends CI_Model{
 		}
 
 	/* obtener las notificaciones de un usuario */
-	public function get_from_licitacion($user_id){
-		$q = "SELECT * FROM notificaciones where user_id=$user_id order by id desc";
+	public function get_from_licitacion($licitacion_id){
+		$q = "SELECT * FROM eventos where licitacion_id=$licitacion_id order by id desc";
 		$query = $this->db->query($q);
         return $query->result();
 	}
